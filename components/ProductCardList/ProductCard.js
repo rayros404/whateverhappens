aaa4281d56f14087c2fe7c78ba33a5cddb5ea7a7
rayros404 <ray.ros404@gmail.com>
2022-04-25 22:36:59 -0700

    productcard images

--- components/ProductCardList/ProductCard.js
+++ components/ProductCardList/ProductCard.js
@@ -9,7 +9,7 @@ const ProductCard = (props) => {
         <CardActionArea>
           <CardMedia
             component="img"
-            image="https://media-photos.depop.com/b0/21750200/1211699845_fd4690a7060243d0b571796f1a0b125e/P0.jpg"
+            image={props.img}
           />
           <Typography className={`titleText ${styles.cardTitle}`}>{props.title}</Typography>
         </CardActionArea>
--- components/ProductCardList/ProductCardList.js
+++ components/ProductCardList/ProductCardList.js
@@ -6,9 +6,18 @@ import { Grid } from "@mui/material"
 const ProductCardList = () => {
   return (
     <Grid container spacing={2} sx={{mb: 2}}>
-      <ProductCard title="New"/>
-      <ProductCard title="Sale"/>
-      <ProductCard title="Explore"/>
+      <ProductCard 
+        title="New" 
+        img="https://media-photos.depop.com/b0/21750200/1170024057_1b71f0951c8b4eedaf2cab63087a0574/P0.jpg"
+      />
+      <ProductCard 
+        title="Sale" 
+        img="https://media-photos.depop.com/b0/21750200/1195057942_814b4e733a054b1bb3b19899dab7d304/P0.jpg"
+      />
+      <ProductCard 
+        title="Explore" 
+        img="https://media-photos.depop.com/b0/21750200/1191397536_13075cb6acad426fa7ecc9fe1061a130/P0.jpg"
+      />
     </Grid>
   )
 }
