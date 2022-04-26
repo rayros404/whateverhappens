8ef530b481c6bfbc5328f98a3d042d44ff8ee112
rayros404 <ray.ros404@gmail.com>
2022-04-26 12:47:50 -0700

    footer desktop

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,46 +2,87 @@ import styles from "./Footer.module.css"
 import { Container, Typography, Grid } from "@mui/material"
 import Socials from "../Navbar/Socials"
 import FooterLink from "./FooterLink"
+import EmailIcon from '@mui/icons-material/Email';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Footer = () => {
   return (
-    <div className={`${styles.container} foreground`}>
-      <Container sx={{display:"flex"}}>
-        <div className={`${styles.content} ${styles.left}`}>
-          <Typography variant="h5" className="titleText textPrimary" sx={{fontWeight: "bold", letterSpacing: "6px", fontSize:"30px"}} gutterBottom>
-            WHATEVERHAPPENS
-          </Typography>
-          <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci sunt, quis rem maiores doloribus tempora, nostrum tempore aperiam veritatis</Typography>
-        </div>
-        <div className={`${styles.content} ${styles.center}`}>
-          <Container>
-          <Typography variant="h6" className="titleText textPrimary" gutterBottom>Shop</Typography>
-          <div className={styles.wrapper}>
-            <ul style={{display:"flex", flexDirection:"column", flexWrap:"wrap", height:"100px"}}>
-              <FooterLink title="All"/>
-              <FooterLink title="New"/>
-              <FooterLink title="Sale"/>
-              <FooterLink title="Tops"/>
-              <FooterLink title="Bottoms"/>
-              <FooterLink title="Dresses"/>
-              <FooterLink title="Intimates"/>
-              <FooterLink title="Shoes"/>
-              <FooterLink title="Accessories"/>
-            </ul>
-          </div>
-          </Container>
-        </div>
-        
-        <div className={`${styles.content} ${styles.right}`}>
-          <Container>
-          <Typography variant="h6" className="titleText textPrimary">Contact</Typography>
-          
-            <Socials />
-
-          </Container>
-        </div>
+    <footer className={`foreground ${styles.footer}`}>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Container>
+              <Typography variant="h4" className="titleText textPrimary" gutterBottom>Info</Typography>
+              <ul>
+                <FooterLink title="About"/>
+                <FooterLink title="FAQ"/>
+                <FooterLink title="Shipping"/>
+                <FooterLink title="Returns"/>
+                <FooterLink title="Terms of Service"/>
+                <FooterLink title="Privacy Policy"/>
+                <FooterLink title="Cookie Policy"/>
+              </ul>
+            </Container>
+          </Grid>
+          <Grid item xs={4}>
+            <Container>
+              <Typography variant="h4" className="titleText textPrimary" gutterBottom>Shop</Typography>
+              <ul>
+                <FooterLink title="All"/>
+                <FooterLink title="New"/>
+                <FooterLink title="Sale"/>
+                <FooterLink title="Tops"/>
+                <FooterLink title="Bottoms"/>
+                <FooterLink title="Dresses"/>
+                <FooterLink title="Intimates"/>
+                <FooterLink title="Shoes"/>
+                <FooterLink title="Acccessories"/>
+              </ul>
+            </Container>
+          </Grid>
+          <Grid item xs={4}>
+            <Container>
+              <Typography variant="h4" className="titleText textPrimary" gutterBottom>Contact</Typography>
+              <div style={{display:"flex", alignItems:"center"}}>
+                <EmailIcon className="textPrimary"/>
+                <Typography 
+                  variant="h6"
+                  className="titleText textPrimary" 
+                  sx={{marginLeft:"10px"}}
+                  gutterBottom
+                >
+                  joebruin@ucla.edu
+                </Typography>
+              </div>
+              <div style={{display:"flex", alignItems:"center"}}>
+                <LocalPhoneIcon className="textPrimary"/>
+                <Typography 
+                  variant="h6"
+                  className="titleText textPrimary" 
+                  sx={{marginLeft:"10px"}}
+                  gutterBottom
+                >
+                  (123) 456-7890
+                </Typography>
+              </div>
+              <div style={{display:"flex", alignItems:"center"}}>
+                <LocationOnIcon className="textPrimary"/>
+                <Typography 
+                  variant="h6"
+                  className="titleText textPrimary" 
+                  sx={{marginLeft:"10px"}}
+                  gutterBottom
+                >
+                  12345 Six St, Los Angeles 78910
+                </Typography>
+              </div>
+              <Socials/>
+            </Container>
+          </Grid>
+        </Grid>
       </Container>
-    </div>
+    </footer>
   )
 }
 
